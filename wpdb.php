3e7f5c3ee8d3e0971259dad514db20c1f2494b4d
Mahdi Valipoor <mahdivalipoornew@gmail.com>
2024-08-06 22:41:52 +0330

    Update wpdb.php

--- wpdb.php
+++ wpdb.php
@@ -16,32 +16,116 @@
 defined('ABSPATH') or die;
 
 if (!class_exists('DbCall')) {
-    class DbCall
-    {        
+    class DbCall 
+    {      
         public function __construct()
         {
-            $this->show();
+            add_action( 'admin_menu', array($this,'registerWpdbAdminMenu') );
+            add_action( 'admin_init', array($this,'registerWpdbSettings') );
+            add_action( 'admin_init', array($this,'insert_data') );
+            register_activation_hook( __FILE__ , [ $this, 'create_table' ] );
         }
-        
-        public function insert() {
-            global $wpdb;
-            $table_name = $wpdb->prefix . 'posts';
 
-            //check wp_insert_post() function
-            $data = array(
-                'post_title' => 'Example Post',
-                'post_content' => 'Decription',
+        public function registerWpdbAdminMenu() {
+            add_menu_page(
+                'wpdb',
+                'wpdb',
+                'manage_options',
+                'wpdb',
+                array($this,'WPDBpage')
             );
+        }
 
-            $success = $wpdb->insert($table_name, $data);
+        public function WPDBpage() {
+            ?>
 
-            if ($success) {
-                $lastID = $wpdb->insert_id;
-            } else {
-                $lastID = 'ERR';
+            <div class="wrap">
+                <h1>WPDB</h1>
+                <form action="options.php" method="post" novalidate="novalidate">
+                        <?php settings_fields( 'wpdb_settings' ); ?>
+                        <table class="form-table" role="presentation">
+                            <?php do_settings_fields( 'wpdb_settings', 'default' ); ?>
+                        </table>
+                        <?php submit_button(); ?>
+                </form>
+            </div>
+
+            <?php
+                $this->show();
+        }
+
+        public function registerWpdbSettings() {
+            $NameArgs = array(
+                array(
+                'type' => 'string',
+                'default' => NULL,
+                ),
+                array(
+                    'type' => 'number',
+                    'default' => NULL,
+                )
+            );
+            
+            register_setting('wpdb_settings', 'wpdb_field_1', $NameArgs[0]);
+            register_setting('wpdb_settings', 'wpdb_field_2', $NameArgs[1]);
+
+
+            add_settings_field( 'wpdb_field_1', esc_html__( 'Full Name', 'default' ), [$this,'wpdb_settings_field_callback'], 'wpdb_settings' );
+            add_settings_field( 'wpdb_field_2', esc_html__( 'Age', 'default' ), [$this,'wpdb_settings_field_callback2'], 'wpdb_settings' );
+        }
+
+        public function wpdb_settings_field_callback() {
+            $name = get_option( 'wpdb_field_1' );
+            echo '<input type="text" name="wpdb_field_1" value="' . esc_attr( $name ) . '">';
+            
+        }
+
+        public function wpdb_settings_field_callback2(){
+            $age = get_option( 'wpdb_field_2' );
+            echo '<input type="number" name="wpdb_field_2" value="' . esc_attr( $age ) . '">';
+        }
+
+        public function insert_data() {
+            if(isset($_POST['submit'])){
+                global $wpdb;
+                $name = get_option( 'wpdb_field_1' );
+                $age = get_option( 'wpdb_field_2' );
+
+
+
+                $table_name = $wpdb->prefix . 'data_call';
+
+                //check wp_insert_post() function
+                $data = array(
+                    'FullName' => $_POST['wpdb_field_1'],
+                    'Age' => $_POST['wpdb_field_2'],
+                );
+
+                $wpdb->insert($table_name, $data);
+
+                // $done var
             }
+        }
 
-            echo 'last id:' . $lastID;
+        /*
+            CREATE TABLE
+        */
+        function create_table() {
+            global $wpdb;
+        
+            $table_name = $wpdb->prefix . 'data_call';
+        
+            $charset_collate = $wpdb->get_charset_collate();
+        
+            $sql = "CREATE TABLE $table_name (
+                id int NOT NULL AUTO_INCREMENT,
+                FullName varchar(255) NOT NULL,
+                Age int,
+                PRIMARY KEY (id)
+            ) $charset_collate;";
+        
+            require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+            dbDelta( $sql );
         }
 
         public function show() {
@@ -49,90 +133,15 @@ if (!class_exists('DbCall')) {
 
             // show errors
             $wpdb->show_errors();
-
+            $table_name = $wpdb->prefix . 'data_call';
             $posts = $wpdb->get_results(
-                "SELECT post_title FROM $wpdb->posts
-                 WHERE post_status = 'publish'
-                 AND post_type = 'post'
-                 ORDER BY post_date ASC LIMIT 0,4"
-                 );
+                "SELECT * FROM $table_name"
+                );
 
-            var_dump($posts);
+            foreach ($posts as $post) {
+               echo $post->FullName . " | " . $post->Age . "<br/>";
+            }
         }
-
-        public function show_row() {
-            global $wpdb;
-
-            // show errors
-            $wpdb->show_errors();
-
-            $row = $wpdb->get_row(
-                "SELECT * FROM $wpdb->posts
-                 WHERE post_status = 'publish'
-                 AND post_type = 'post'
-                 ORDER BY comment_count DESC LIMIT 0,1"
-                 );
-
-            var_dump($row);
-        }
-
-        public function show_col() {
-            global $wpdb;
-
-            // show errors
-            $wpdb->show_errors();
-
-            $cols = $wpdb->get_col(
-                "SELECT post_title FROM $wpdb->posts
-                 WHERE post_status = 'publish'
-                 AND post_type = 'post'
-                 ORDER BY comment_count DESC LIMIT 0,4"
-                 );
-
-            var_dump($cols);
-        }
-
-        public function show_var() {
-            global $wpdb;
-
-            // show errors
-            $wpdb->show_errors();
-
-            $regis_date = $wpdb->get_var(
-                "SELECT user_registered FROM $wpdb->users
-                 WHERE user_login = 'root'"
-                 );
-
-            var_dump($regis_date);
-        }
-
-        public function prepare_test() {
-            global $wpdb;
-
-            // show errors
-            $wpdb->show_errors();
-
-            $user = 'root';
-            $regis_date = $wpdb->prepare(
-                "SELECT user_registered FROM $wpdb->users
-                 WHERE user_login = %s",$user
-                 );
-            
-            $res = $wpdb->get_var($regis_date);
-
-            var_dump($res);
-        }
-
-        // $wpdb->insert
-        // $wpdb->update
-        // $wpdb->delete
-
-        // $wpdb->num_rows
-        // $wpdb->rows_affected
-        // $wpdb->insert_id
-        // $wpdb->prefix
-        // $wpdb->{core tables}
-
     }
     
     $restCall = new DbCall;
